e929835c49fcb6c2dd836ea77355212bc0dae505
liqiong_t430 <liqiong456@126.com>
2016-09-10 11:06:26 +0800

    Signed-off-by: liqiong_t430 <liqiong456@126.com>

--- uitest/src/main/java/com/example/uitest/OtherActivity.java
+++ uitest/src/main/java/com/example/uitest/OtherActivity.java
@@ -1,26 +1,29 @@
 package com.example.uitest;
 
-import android.animation.LayoutTransition;
-import android.app.Activity;
+import android.animation.Animator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.transition.Transition;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.ViewAnimationUtils;
 import android.view.animation.Animation;
+import android.widget.ImageView;
 
 /**
  * Created by Administrator on 2016/9/9.
  */
 public class OtherActivity extends AppCompatActivity {
-
+    private ImageView iv_main;
+    private CardView cardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
-        CardView cardView = (CardView) findViewById(R.id.cardlist_item);
+        cardView = (CardView) findViewById(R.id.cardlist_item);
+        iv_main = (ImageView) findViewById(R.id.iv_main);
+
         getWindow().getSharedElementEnterTransition().addListener(new Transition.TransitionListener() {
             @Override
             public void onTransitionStart(Transition transition) {
@@ -31,6 +34,7 @@ public class OtherActivity extends AppCompatActivity {
             @Override
             public void onTransitionEnd(Transition transition) {
                 Log.e("liqiong", "onTransitionEnd");
+                animClick();
             }
 
             @Override
@@ -48,65 +52,32 @@ public class OtherActivity extends AppCompatActivity {
                 Log.e("liqiong", "onTransitionResume");
             }
         });
-//        cardView.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
-//            @Override
-//            public void onLayoutChange(View view, int i, int i1, int i2, int i3, int i4, int i5, int i6, int i7) {
-//                Log.e("liqiong", view.toString());
-//                Log.e("liqiong", i+"");
-//                Log.e("liqiong", i1+"");
-//                Log.e("liqiong", i2+"");
-//                Log.e("liqiong", i3+"");
-//                Log.e("liqiong", i4+"");
-//                Log.e("liqiong", i5+"");
-//                Log.e("liqiong", i6+"");
-//                Log.e("liqiong", i7+"");
-//            }
-//        });
+    }
 
-//        cardView.setLayoutAnimationListener(new Animation.AnimationListener() {
-//            @Override
-//            public void onAnimationStart(Animation animation) {
-//                Log.e("liqiong","onAnimationStart");
-//            }
-//
-//            @Override
-//            public void onAnimationEnd(Animation animation) {
-//                Log.e("liqiong","onAnimationEnd");
-//            }
-//
-//            @Override
-//            public void onAnimationRepeat(Animation animation) {
-//                Log.e("liqiong","onAnimationRepeat");
-//            }
-//        });
+    public void animClick() {
+        Animator animator= ViewAnimationUtils.createCircularReveal(iv_main,500,500,20,2000);
+        animator.setDuration(500);
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+                iv_main.setVisibility(View.VISIBLE);
+            }
 
-//        cardView.getLayoutTransition().addTransitionListener(new LayoutTransition.TransitionListener() {
-//            @Override
-//            public void startTransition(LayoutTransition layoutTransition, ViewGroup viewGroup, View view, int i) {
-//                Log.e("liqiong","startTransition");
-//            }
-//
-//            @Override
-//            public void endTransition(LayoutTransition layoutTransition, ViewGroup viewGroup, View view, int i) {
-//                Log.e("liqiong","endTransition");
-//            }
-//        });
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                cardView.setVisibility(View.GONE);
+            }
 
-//        cardView.getAnimation().setAnimationListener(new Animation.AnimationListener() {
-//            @Override
-//            public void onAnimationStart(Animation animation) {
-//                Log.e("liqiong","onAnimationStart");
-//            }
-//
-//            @Override
-//            public void onAnimationEnd(Animation animation) {
-//                Log.e("liqiong","onAnimationEnd");
-//            }
-//
-//            @Override
-//            public void onAnimationRepeat(Animation animation) {
-//                Log.e("liqiong","onAnimationRepeat");
-//            }
-//        });
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
+        animator.start();
     }
 }
