a9091e8ceeb5d9e3ff480d0c2d329f8eb2b7f544
liqiong_t430 <liqiong456@126.com>
2016-09-25 11:16:39 +0800

    Signed-off-by: liqiong_t430 <liqiong456@126.com> Android Permission

--- uitest/src/main/java/com/example/uitest/MainActivity.java
+++ uitest/src/main/java/com/example/uitest/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.uitest;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.view.View;
@@ -14,6 +19,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private CardView cardView = null;
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startOtherActivity(View view) {
-        Intent mIntent = new Intent(this, OtherActivity.class);
-        startActivity(mIntent, ActivityOptions.makeSceneTransitionAnimation(this, cardView, cardView.getTransitionName()).toBundle());
+//        Intent mIntent = new Intent(this, OtherActivity.class);
+//        startActivity(mIntent, ActivityOptions.makeSceneTransitionAnimation(this, cardView, cardView.getTransitionName()).toBundle());
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CAMERA)){
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+            }else{
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+            }
+        } else {
+            Toast.makeText(this, "!!!!!!WWWWWWWWWWWWW", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_CAMERA) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "permission OK!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "permission Denied", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
